97e1dbe84366d21ac6f7d817e58e8bac0549c9ab
Rendich <rarendic@uc.cl>
2020-12-08 17:22:00 -0300

    3 show/hide rows table

--- public/libs/packages.js
+++ public/libs/packages.js
@@ -41,10 +41,10 @@
         if (d.imports) d.imports.forEach(function(I) {
           i = I[1]
           // AQUI
-          console.log(I)
-          console.log(d)
+          //console.log(I)
+          //console.log(d)
           //console.log(i)
-          console.log("-----")
+          //console.log("-----")
           //imports.push({source: map[d.name], target: map[i], relation: d.relation});
           imports.push({source: map[d.name], target: map[i], relation: I[0]});
           //imports.push({source: map[d.name], target: map[i]});
