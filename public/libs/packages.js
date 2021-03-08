99b4cdf5f51a6177d02bdddc653fd2b3b9cc4ae5
Rendich <rarendic@uc.cl>
2021-03-07 22:49:39 -0300

    refactoring

--- public/libs/packages.js
+++ public/libs/packages.js
@@ -1,16 +1,13 @@
 (function() {
   packages = {
-
     // Lazily construct the package hierarchy from class names.
     root: function(classes) {
       var map = {};
-
       function find(name, data) {
         var node = map[name], i;
         if (!node) {
           node = map[name] = data || {name: name, children: []};
           if (name.length) {
-            //node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
             node.parent = find(name.substring(0, i = name.lastIndexOf("*")));
             node.parent.children.push(node);
             node.key = name.substring(i + 1);
@@ -18,41 +15,27 @@
         }
         return node;
       }
-
       classes.forEach(function(d) {
         find(d.name, d);
       });
-
       return map[""];
     },
-
     // Return a list of imports for the given array of nodes.
     imports: function(nodes) {
       var map = {},
           imports = [];
-
       // Compute a map from name to node.
       nodes.forEach(function(d) {
         map[d.name] = d;
       });
-
       // For each import, construct a link from the source to target node.
       nodes.forEach(function(d) {
         if (d.imports) d.imports.forEach(function(I) {
           i = I[1]
-          // AQUI
-          //console.log(I)
-          //console.log(d)
-          //console.log(i)
-          //console.log("-----")
-          //imports.push({source: map[d.name], target: map[i], relation: d.relation});
           imports.push({source: map[d.name], target: map[i], relation: I[0]});
-          //imports.push({source: map[d.name], target: map[i]});
         });
       });
-
       return imports;
     }
-
   };
 })();
